448bab944d4ed3fb3067eb3e3be93d43a6945340
hebelia <heberomeu@gmail.com>
2023-10-22 19:22:59 -0300

    feat(Footer): added svg wave and a logo on the mobile menu

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,44 +12,62 @@ import "./footer.css";
 const Footer = () => {
   return (
     <div className="footerContainer">
-      <div className="footerTop">
-        <div className="footerTopLeftWrapper">
-          <h3 className="h3footer">Información</h3>
+      <div className="footerInfo">
 
-          <Link to="/contacto">Contacto</Link>
-          <Link to="/nosotros">Nosotros</Link>
-          <Link to="/preguntas-frecuentes">Preguntas frecuentes</Link>
-          <Link to="/products">Productos</Link>
-          <Link to="/">Inicio</Link>
-        </div>
-        <div>
-          <h3 className="h3footer">Redes Sociales</h3>
-          <a
-            href="https://www.instagram.com/ladyfitlovers/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <InstagramOutlined /> Instagram
-          </a>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#e0b3d3"
+              fill-opacity="1"
+              d="M0,256L80,224C160,192,320,128,480,122.7C640,117,800,171,960,197.3C1120,224,1280,224,1360,224L1440,224L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+              data-darkreader-inline-fill=""
+              // style="--darkreader-inline-fill: #b9006e;"
+            ></path>
+          </svg>
 
-          <h3 className="h3footer">Contactanos</h3>
-          <a href="mailto:ladyfitlovers@gmail.com" style={{ color: "#ba338a" }}>
-            <Link>
-              <MailOutlined></MailOutlined> LadyFitLovers@gmail.com
+        {/* RESTO DEL FOOTER------------------- */}
+        <div className="footerTop">
+          <div className="footerTopLeftWrapper">
+            <h3 className="h3footer">Información</h3>
+
+            <Link to="/contacto">Contacto</Link>
+            <Link to="/nosotros">Nosotros</Link>
+            <Link to="/preguntas-frecuentes">Preguntas frecuentes</Link>
+            <Link to="/products">Productos</Link>
+            <Link to="/">Inicio</Link>
+          </div>
+          <div>
+            <h3 className="h3footer">Redes Sociales</h3>
+            <a
+              href="https://www.instagram.com/ladyfitlovers/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <InstagramOutlined /> Instagram
+            </a>
+
+            <h3 className="h3footer">Contactanos</h3>
+            <a
+              href="mailto:ladyfitlovers@gmail.com"
+              style={{ color: "#ba338a" }}
+            >
+              <Link>
+                <MailOutlined></MailOutlined> LadyFitLovers@gmail.com
+              </Link>
+            </a>
+            <br></br>
+            <Link to="https://chat.whatsapp.com/LijG0wDr4ytLMZvYiU1dra">
+              <WhatsAppOutlined /> Whatsapp
             </Link>
-          </a>
-          <br></br>
-          <Link to="https://chat.whatsapp.com/LijG0wDr4ytLMZvYiU1dra">
-            <WhatsAppOutlined /> Whatsapp
-          </Link>
-        </div>
+          </div>
 
-        <div>
-          <h3 className="h3footer">Ciudad</h3>
-          <p style={{ color: "#ba338a" }}>
-            <EnvironmentOutlined /> Berazategui, Bs.As. Arg.
-          </p>
+          <div>
+            <h3 className="h3footer">Ciudad</h3>
+            <p style={{ color: "#ba338a" }}>
+              <EnvironmentOutlined /> Berazategui, Bs.As. Arg.
+            </p>
+          </div>
         </div>
+        {/* RESTO DEL FOOTER------------------- */}
       </div>
       <div className="footerBottom">
         ©2023 Todos los derechos reservados -
