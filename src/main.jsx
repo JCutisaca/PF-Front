3ce1103e4ea859d4289f3f26d88e0472790ff119
soldier_lucas <3jhoncuco@gmail.com>
2023-10-23 08:03:01 -0300

    fixed deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom'
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
@@ -8,14 +8,11 @@ import store from "./redux/store";
 
 // import "antd/dist/antd.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </Provider>
-
-  // <Router>
-  //   <App />
-  // </Router>
+  </Provider>,
+  document.getElementById("root")
 );
